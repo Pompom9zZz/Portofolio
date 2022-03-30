e2f829c8719428260d86576cc3cd926a760f1fe5
Fourcaudot Tom <95436636+Pompom9zZz@users.noreply.github.com>
2022-03-30 09:57:31 +0200

    JS Update

--- sketch.js
+++ sketch.js
@@ -1,15 +1,20 @@
 var root = [];
 const NBP = 100;
 
+
+Event.on('resize', function () {
+    setup();
+});
+
 function setup(){
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(windowWidth , windowHeight);
     for (let i = 0; i < NBP; i++) {
         root.push(new Part());
     }
 }
 
 function draw(){
-    background(0);
+    background(0, 0, 0);
     root.forEach(element => {
         element.move();
         element.connect(root);
