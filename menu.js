cb4d631d8b40053aaa92bae17cc52919b71d8c22
tfourcaudot <tom.fourcaudot@etu.u-bordeaux.fr>
2022-04-14 16:29:14 +0200

    Générateur page retex

--- menu.js
+++ menu.js
@@ -15,3 +15,8 @@ function updateMenu() {
         });
     });
 }
+
+function loadRetex(img, title) {
+    var opened = window.open("");
+    opened.document.write("<!DOCTYPE html><html><head> <link href='./Retex.css' rel='stylesheet'><title>RETEX</title></head><body><h1>" + title + "</h1><iframe src='" + img + "'type='application/pdf'/></body></html>");
+}
